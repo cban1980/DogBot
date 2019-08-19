eb003d052e1624128145b6b17131f6aac7e5f361
cban1980 <cban@gmx.com>
2019-08-19 13:25:05 +0200

    initial commit

---
+++ dogbot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+from bs4 import BeautifulSoup as bs
+import os
+
+client = discord.Client()
+
+
+def cssformat(input):
+    return "```css\n" + input + "```"
+
+
+def htmlformat(input):
+    return "```html\n" + input + "```"
+
+
+bot = commands.Bot(command_prefix='!')
+HOMEDIR = os.path.expanduser('~')
+TOKENHOME = "%s/DogBot/" % (HOMEDIR)
+bot.remove_command('help')
+
+with open(TOKENHOME + "token.txt", "r") as readfile:
+    TOKEN = readfile.read().strip()
+
+@bot.command(name='warpop', pass_context=True)
+async def warpop():
+    htmldata = requests.get('https://www.returnofreckoning.com/whos_online.php').text
+    soup = bs(htmldata, 'lxml')
+    pop = soup.find(class_="realm-info realm-info-detail").getText()
+    pop = pop.replace("Total :", "")
+    pop = pop.replace("Faction ratio (Order/Destruction) :", "")
+    pop = pop.replace(":", "")
+    pop = pop.replace("Martyrs Square (EN)", "")
+    pop = pop.strip().splitlines()
+    pop = list(filter(None, pop))
+    pop = '\n'.join(pop)
+    await bot.say(cssformat(str(*pop, sep = "\n")))
+
+
+@client.event
+async def on_ready():
+    print("Connected!")
+    print(client.user.name)
+    print(client.user.id)
+    await client.change_presence(game=discord.Game(name="Doge Of War"))
+
+client.run(TOKEN)
