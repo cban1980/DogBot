8110a2a44f5f9eae799d0329118307066500349f
cban1980 <48292491+cban1980@users.noreply.github.com>
2019-08-24 19:53:15 +0200

    created iniformat function.

--- dogbot.py
+++ dogbot.py
@@ -12,6 +12,10 @@ def cssformat(input):
     return "```css\n" + input + "```"
 
 
+def iniformat(input):
+    return "```ini\n" + input + "```"
+
+
 def htmlformat(input):
     return "```html\n" + input + "```"
 
@@ -79,7 +83,7 @@ async def dice(ctx, arg, arg1):
         number = []
         for i in range(1, int(arg)+1):
             number.append(random.randint(min, max))
-    await bot.say("The dice(s) tumbles and rolls for " + ctx.message.author.mention + " and it gives the numbers: " + htmlformat(str(number)))
+    await bot.say("The dice(s) tumbles and rolls for " + ctx.message.author.mention + " and it gives the numbers: " + iniformat(str(number)))
     
 
 
