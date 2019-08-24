fb847180d59dd4eca3478220a2c8580f7badaa7f
cban1980 <48292491+cban1980@users.noreply.github.com>
2019-08-24 22:25:57 +0200

    cumulative dice score

--- dogbot.py
+++ dogbot.py
@@ -83,7 +83,8 @@ async def dice(ctx, arg, arg1):
         number = []
         for i in range(1, int(arg)+1):
             number.append(random.randint(min, max))
-    await bot.say("The dice(s) tumbles and rolls for " + ctx.message.author.mention + " and they gives the numbers: " + iniformat(str(number)))
+    total = sum(number)
+    await bot.say("The dice(s) tumbles and rolls for " + ctx.message.author.mention + " and they gives the numbers: " + iniformat(str(number)) + " Total: " + total)
     
 
 
