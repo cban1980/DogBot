a0dc6b4feee1747c0a7f016c302f30f2e21dfc36
cban1980 <48292491+cban1980@users.noreply.github.com>
2019-08-24 19:24:11 +0200

    bug

--- dogbot.py
+++ dogbot.py
@@ -78,8 +78,8 @@ async def dice(ctx, arg, arg1):
     else:
         for i in range(1, int(arg)+1):
             number.append(random.randint(min, max))
-await bot.say("The dice(s) tumbles and rolls for " + ctx.message.author.mention + " and it gives the numbers:
-await.bot.say(cssformat(bold(str(number))))
+    await bot.say("The dice(s) tumbles and rolls for " + ctx.message.author.mention + " and it gives the numbers:")
+    await.bot.say(cssformat(bold(str(number))))
 
 
 @bot.command(name='serverinvite', pass_context=True)
